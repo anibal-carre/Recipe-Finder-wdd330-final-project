dea2cdc7f4f2c3fab4e6d6a09ce22aabea0250a0
anibal-carre <anibaldavidcarrenoalvarez@gmail.com>
2025-06-12 20:58:35 -0300

    Reorganize project structure

--- src/main.js
+++ src/main.js
@@ -1,14 +1,1 @@
-import { header } from "./components";
-document.querySelector("#app").innerHTML = `
-  <div>
-    ${header}
 
-    <div class="container">
-      <div class="main-content">
-      
-      </div>
-    </div>
-  </div>
-`;
-
-setupCounter(document.querySelector("#counter"));
